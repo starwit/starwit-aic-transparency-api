173e3f6b72a3671f168959a2cd74dae85bf1e570
Anett Hübner <anett_huebner@gmx.de>
2025-05-30 21:32:08 +0200

    use import file to import modules

--- src/main/java/de/starwit/service/InitializationService.java
+++ src/main/java/de/starwit/service/InitializationService.java
@@ -2,8 +2,6 @@ package de.starwit.service;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -16,7 +14,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.starwit.aic.model.Module;
-import de.starwit.aic.model.ModuleSBOMLocationValue;
 import jakarta.annotation.PostConstruct;
 
 @Service
@@ -38,10 +35,8 @@ public class InitializationService {
     @Value("${scenario.setup:false}")
     private boolean setupScenario = true;
 
-    @Value("${scenario.importFolder:internal}")
-    private String scenarioImportFolder;
-
-    private String sampleDataFileName = "moduledata.json";
+    @Value("${scenario.import.location:import-data/modules.json}")
+    private String scenarioImportLocation;
 
     @Autowired
     ObjectMapper mapper;
@@ -50,45 +45,15 @@ public class InitializationService {
     public void init() {
         if (setupScenario) {
             log.info("Setting up scenario data from env vars");
-            if ("internal".equals(scenarioImportFolder)) {
-                log.info("Load pre-packaged module data, check if this is intended!");
-                loadPrePackagedDemoData();
-            } else {
-                log.info("Load scenario data");
-                loadScenarioData();
-            }
+            log.info("Load scenario data");
+            loadScenarioData();
         } else {
             log.info("start empty transparency API, modules must register!");
         }
     }
 
-    private void loadPrePackagedDemoData() {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("sampledata.json");
-        try {
-            Module[] mods = mapper.readValue(inputStream, Module[].class);
-            updateUris(mods);
-            sendModuleDataToCockpit(Arrays.asList(mods));
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            System.exit(1);
-        }
-    }
-
-    private void updateUris(Module[] mods) {
-        for (Module module : mods) {
-            for (String key : module.getsBOMLocation().keySet()) {
-                ModuleSBOMLocationValue locValue = module.getsBOMLocation().get(key);
-                var uri = locValue.getUrl();
-                if (!uri.contains("http")) {
-                    locValue.setUrl(serviceUri + "/" + uri);
-                    module.getsBOMLocation().put(key, locValue);
-                }
-            }
-        }
-    }
-
     private void loadScenarioData() {
-        File file = new File(scenarioImportFolder + "/" + sampleDataFileName);
+        File file = new File(scenarioImportLocation);
         if (file.exists()) {
             log.info("Loading scenario data from {}", file.getAbsolutePath());
             try {
