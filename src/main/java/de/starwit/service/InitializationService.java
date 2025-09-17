6cd1897c1dc62ab43cdb78f3babacc80b6f79530
ztarbug <markus.zarbock@starwit.de>
2025-09-17 15:01:15 +0200

    fix param name

--- src/main/java/de/starwit/service/InitializationService.java
+++ src/main/java/de/starwit/service/InitializationService.java
@@ -35,7 +35,7 @@ public class InitializationService {
     @Value("${scenario.setup:false}")
     private boolean setupScenario = true;
 
-    @Value("${scenario.importLocation:import-data/moduledata.json}")
+    @Value("${scenario.importFolder:import-data/moduledata.json}")
     private String scenarioImportLocation;
 
     @Autowired
